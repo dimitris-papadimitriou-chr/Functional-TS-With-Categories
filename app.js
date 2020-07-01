8593e76b6498a57f151005b21d3cb0d2d971a76b
dimit <dimit@DESKTOP-T6V9AM2>
2020-07-01 19:52:24 +0300

    gitignore

--- app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const maybe_example_1 = require("./Ch03/maybe.example");
-maybe_example_1.demo();
-//# sourceMappingURL=app.js.map
